fd30a08ad70aa8ed34ea3d099fd6ff5f9f258081
Usuario <Usuario@DESKTOP-5JJCS9M>
2021-07-08 01:19:28 -0400

    Ventas

---
+++ src/Tarea.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+
+import java.util.Scanner;
+
+public class Tarea {
+    public static void main (String[] ar){
+        Scanner x=new Scanner(System.in);
+        System.out.print("Ingrese numero:");
+        int Num= x.nextInt();
+        while(Num<101){
+            System.out.print(Num+"\n");
+            Num++;
+}
+}
+}
